16ad47e5d7e37df3024decd2787c80989a3a439c
ben thacker <benthac@gmail.com>
2021-06-01 17:24:00 -0400

    timer and vars

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello");
+var goButton = document.querySelector("#mainButton");
+console.log("goButton");
+goButton.addEventListener("click", function(){
+    alert("I heard that");
+});
+// question slideshow that triggers on click
+// click should also trigger a countdown timer
+var sec = 60;
+var time = setInterval(mytimer, 1000);
+
+function myTimer() {
+    document.getElementById('#timer').innerHTML = sec + "sec left";
+    sec--;
+    if (sec == -1) {
+        clearInterval(time);
+        alert("Game Over!");
+    }
+}
